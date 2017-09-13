ccce349786762dc9430060fef621edd5c671ecee
Wendy Wang <wangwenusa11@gmail.com>
2017-09-12 23:05:25 -0700

    Finished problem5

--- Assignment1.R
+++ Assignment1.R
@@ -1,11 +1,27 @@
 #===============Problem 5(b)=====================================
-channel <- sample(c(1, 2, 3), size=100, replace=TRUE, prob=c(0.5, 0.3, 0.2))
-decode <- sample(c(1, 2, 3), size=100, replace=TRUE, prob=c(0.8, 0.9, 0.7))
+simulateSize <- 100000
+channel <- sample(c(1, 2, 3), size=simulateSize, replace=TRUE, prob=c(0.5, 0.3, 0.2))
 
-channeltable <- table(channel)
+channelTable <- as.data.frame(table(channel))
+channelA <- channelTable[1,]$Freq / simulateSize
+channelB <- channelTable[2,]$Freq / simulateSize
+channelC <- channelTable[3,]$Freq / simulateSize
 
+decodeA <- sample(c(1, 0), size=simulateSize, replace=TRUE, prob=c(0.8, 0.2))
+decodeB <- sample(c(1, 0), size=simulateSize, replace=TRUE, prob=c(0.8, 0.2))
+decodeC <- sample(c(1, 0), size=simulateSize, replace=TRUE, prob=c(0.8, 0.2))
+decodeTableA <- as.data.frame(table(decodeA))
+decodeTableB <- as.data.frame(table(decodeB))
+decodeTableC <- as.data.frame(table(decodeC))
+ProbDecodeA <- decodeTableA[2,]$Freq / simulateSize
+ProbDecodeB <- decodeTableB[2,]$Freq / simulateSize
+ProbDecodeC <- decodeTableC[2,]$Freq / simulateSize
 
-
+P_A <- channelA * ProbDecodeA
+P_B <- channelB * ProbDecodeB
+P_C <- channelC * ProbDecodeC
+P_total <- P_A + P_B + P_C
+P_total
 
 #===============Problem 9 ========================================
 for (i in 1:50){
