596fe66cfef8792e0f41159b9343e71ab53c9e0d
Wendy Wang <wangwenusa11@gmail.com>
2017-09-15 12:56:58 -0700

    Change the file name

---
+++ HW1_9.R
@@ -0,0 +1,26 @@
+#===============Problem 9 ========================================
+simulation <- 10
+for(i in seq(from=0, to=simulation, by=1)){
+  paulLostCounter <- 0
+  paul <- 50
+  linda <- 50
+  #if dice == 1, then it's head; if dice == 0, then it's tail
+  while (paul > 0 && linda > 0){
+    dice <- sample(c(0,1), size=1, replace=TRUE, prob=c(0.5,0.5)) #flip the coin
+    if (dice == 0){ #tail, Linda wins
+      paul <- paul - 2
+      linda <- linda + 2
+    } 
+    else if (dice == 1) { #head, Paul wins
+      linda <- linda - 2
+      paul <- paul + 2
+    } 
+  }
+  if (paul <= 0) { 
+    paulLostCounter <- paulLostCounter + 1 
+    print(paulLostCounter)
+  }
+  i <- i + 1;
+}
+probPualLost <- paulLostCounter / simulation
+probPualLost
