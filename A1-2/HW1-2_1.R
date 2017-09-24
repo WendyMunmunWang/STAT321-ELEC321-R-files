8a968265663db58054fee182291f43bcfba42ec6
Wendy Wang <wangwenusa11@gmail.com>
2017-09-23 22:55:38 -0700

    HW1-2 Q1 started

---
+++ A1-2/HW1-2_1.R
@@ -0,0 +1,21 @@
+#===============Problem 1.4=====================================
+#use numeration to represent different color in the array
+#1 represents green ball
+#2 represents red ball
+#3 represents blue ball
+urn <- c(1, 1, 1, 1, 1, 1, 
+         2, 2, 2, 2, 2, 2, 
+         3, 3, 3 ,3 ,3, 3, 
+         3, 3)
+
+simulate_size <- 10
+iteration <- simulate_size
+red_count <- 0
+while (iteration > 0){
+  sample_balls <- sample(urn, 3)
+  sample_table <- as.data.frame(table(sample_balls))
+  red_count <- red_count + sample_table[1,]$Freq
+  iteration <- iteration - 1
+}
+probability_red <- red_count / (3 * simulate_size)
+expected_red <- probability_red * 6
