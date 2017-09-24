820ec8dca8da5f4d0f33d153a8b3df4c823801e7
Wendy Wang <wangwenusa11@gmail.com>
2017-09-24 16:06:25 -0700

    Finished problem 6

--- A1-2/HW1-2_6.R
+++ A1-2/HW1-2_6.R
@@ -1,3 +1,12 @@
 #===============Problem 6(b)=====================================
-drawn_chip <- sample(1:100, 5, replace=T)
-drawn_chip
+simulation_size <- 100
+iteration <- 0
+min_vec <- c()
+while (iteration < simulation_size){
+  drawn_chip <- sample(1:100, 5, replace=T)
+  min_vec <- c(min_vec, min(drawn_chip))
+  iteration <- iteration + 1
+}
+mean(min_vec)
+var(min_vec)
+plot(min_vec)
