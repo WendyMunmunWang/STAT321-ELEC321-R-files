ccb461a013c57cd8dec6702677f198f3e2f81179
Wendy Wang <wangwenusa11@gmail.com>
2017-09-26 23:44:53 -0700

    Made changes in Q1-2_1

--- A1-2/HW1-2_1.R
+++ A1-2/HW1-2_1.R
@@ -8,17 +8,21 @@ urn <- c(1, 1, 1, 1, 1, 1,
          3, 3, 3 ,3 ,3, 3, 
          3, 3)
 
-simulate_size <- 100
+simulate_size <- 1000
 iteration <- simulate_size
 red_count <- 0
 red_collect <- c()
 while (iteration > 0){
   sample_balls <- sample(urn, 3)
   sample_table <- as.data.frame(table(sample_balls))
-  red_count <- red_count + sample_table[1,]$Freq
-  red_collect <- c(red_collect, sample_table[1,]$Freq)
+  if (sample_table[1,]$sample_balls == 1){
+    red_count <- red_count + sample_table[1,]$Freq
+    red_collect <- c(red_collect, sample_table[1,]$Freq)
+  }
   iteration <- iteration - 1
 }
-probability_red <- red_count / (3 * simulate_size)
-expected_red <- probability_red * 3
-plot(red_collect)
+red_df <- as.data.frame(table(red_collect)) #convert red_collect to see the frequency for how many red balls are picked
+prob_red_1 <- red_df[1,]$Freq / simulate_size
+prob_red_2 <- red_df[2,]$Freq / simulate_size
+prob_red_3 <- red_df[3,]$Freq / simulate_size
+prob_red_1 * 1 + prob_red_2 * 2 + prob_red_3 * 3
