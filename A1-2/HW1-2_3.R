976c6293d7967d9886ee98a4b4b3907794e84f70
Wendy Wang <wangwenusa11@gmail.com>
2017-09-30 23:27:40 -0700

    Working on Q3

--- A1-2/HW1-2_3.R
+++ A1-2/HW1-2_3.R
@@ -1,13 +1,71 @@
 #===============Problem 3.4=====================================
-tree_size <- 100
-#1 is dead tree is detected by device A
-A <- sample(x=c(1,0), tree_size, 0.8) 
+#Problem 3.1 Validation 
+simulation_size <- 1000
+iteration <- 0
+prob_vec <- c()
+while (iteration < simulation_size){
+  tree_size <- 100
+  #1 is dead tree is detected by device A
+  A <- sample(x=c(1,0), tree_size, 0.8) 
+  
+  #1 is dead tree is detected by device B
+  B <- sample(x=c(1,0), tree_size, 0.9) 
+  index <- 1 #in R, vector index is 1 based
+  freq <- 0
+  while (index <= length(A)){
+    if (B[index] == 1 || A[index] == 1){
+      freq <- freq + 1
+    }
+    index <- index + 1
+  }
+  prob <- freq / tree_size
+  prob_vec <- c(prob_vec, prob)
+  iteration <- iteration + 1
+}
 
-#1 is dead tree is detected by device B
-B <- sample(x=c(1,0), tree_size, 0.9) 
 
-#1 is dead tree location is pinpointed by device A
-A_locate <- sample(x=c(1,0), tree_size, 0.7) 
+#Problem 3.2 Validation 
+iteration <- 0
+prob_one_device <- c()
+while (iteration < simulation_size){
+  tree_size <- 100
+  #1 is dead tree is detected by device A
+  A <- sample(x=c(1,0), tree_size, 0.8) 
+  
+  #1 is dead tree is detected by device B
+  B <- sample(x=c(1,0), tree_size, 0.9) 
+  index <- 1 #in R, vector index is 1 based
+  freq <- 0
+  while (index <= length(A)){
+    if (B[index] == 1 || A[index] == 1){
+      freq <- freq + 1
+    }
+    index <- index + 1
+  }
+  prob <- freq / tree_size
+  prob_one_device <- c(prob_one_device, prob)
+  iteration <- iteration + 1
+}
 
-#1 is dead tree is pinpointed by device B
-B_locate <- sample(x=c(1,0), tree_size, 0.4) 
+
+#Problem 3.3 Validation 
+iteration <- 0
+prob_locvec <- c()
+while (iteration < simulation_size){
+  #1 is dead tree location is pinpointed by device A
+  A_locate <- sample(x=c(1,0), tree_size, 0.7) 
+  
+  #1 is dead tree is pinpointed by device B
+  B_locate <- sample(x=c(1,0), tree_size, 0.4) 
+  index <- 1 #in R, vector index is 1 based
+  freq <- 0
+  while (index <= length(A)){
+    if (B[index] == 1 && A[index] == 1){
+      freq <- freq + 1
+    }
+    index <- index + 1
+  }
+  prob <- freq / tree_size
+  prob_locvec <- c(prob_locvec, prob)
+  iteration <- iteration + 1
+}
