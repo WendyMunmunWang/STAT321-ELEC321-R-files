7422a278db930d0260d56ff1e8fef90f953fb533
Wendy Wang <wangwenusa11@gmail.com>
2017-10-06 23:29:06 -0700

    Finished Q4

--- A2-1/A2-1p4.R
+++ A2-1/A2-1p4.R
@@ -2,3 +2,8 @@
 lambda <- 42
 p_accumulative <- ppois(45, lambda)
 1-p_accumulative
+
+#(c)
+p_160 <- pbinom(160, size = 200, prob = 0.7745)
+p_140 <- pbinom(140, size = 200, prob = 0.7745)
+p <- p_160 - p_140
